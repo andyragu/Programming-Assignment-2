5f78dcc49d4d3b32a13cb2a725f2e0c1bc343bd6
Timothy <troch008@fiu.edu>
2025-02-20 00:24:51 -0500

    Working on step 1 Client Side

--- Updated_Assignment_2_Resources_Download/Deliverables/CODE/client.c
+++ Updated_Assignment_2_Resources_Download/Deliverables/CODE/client.c
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mqueue.h>
+#include <pthread.h>
 
 #define QUEUE_NAME "/server_queue"
 #define MAX_MSG_SIZE 1024
+#define SEND_QUEUE "/server_queue"
+#define RECEIVE_QUEUE "/client_queue"
+ 
+void* thread_manager(void* arg)
+{
+    mqd_t mq_received;
+    char buffer[MAX_MSG_SIZE]; //Store the messages. Storage designated to max size var
+
+    mq_received = mq_open(RECEIVE_QUEUE, O_RDONLY);
+    if (mq_received == (mqd_t)-1) {
+        perror("mq_open failed to be received");
+        pthread_exit(NULL); // Terminates current thread (but allows other threads to keep running)
+        return NULL;
+    }
+}
 
 int main() {
     mqd_t mq;
@@ -14,6 +30,19 @@ int main() {
     if (mq == (mqd_t)-1) {
         perror("mq_open failed");
         exit(1);
+    }
+
+    receiver_thread()
+    {
+        pthread_t thread_ID = 001;
+        int result = pthread_create(&thread_ID, NULL, thread_manager, NULL); 
+
+        //Check for success
+        if (result != 0)
+        {
+            perror("Failed to create thread");
+            exit(1)
+        }
     }
 
     while (1) {
